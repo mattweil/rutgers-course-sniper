d37c0a755b9b2cc5e0acedc621935391fc8297bd
mattweil <matt.weil@rutgers.edu>
2018-08-13 19:02:18 -0400

    IU-2018.2 <mattweil@nbp-23-188.nbp.ruw.rutgers.edu Overwrite remote https://github.com/mattweil/rutgers-course-sniper to local

--- src/main/java/Configuration.java
+++
@@ -1,7 +0,0 @@
-public class Configuration {
-    public static int refreshRate = 10000; //rate at which the sniper checks for open sections in milliseconds
-    public static String campus = "NB";
-    public static int year;
-    public static int term;
-
-}
